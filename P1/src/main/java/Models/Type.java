d232b1d7343bba172f89c486de019ba51c0fb489
Kinga360 <jjadrian1@hotmail.com>
2022-05-09 04:41:55 -0400

    enum update

--- P1/src/main/java/Models/Type.java
+++ P1/src/main/java/Models/Type.java
@@ -2,4 +2,15 @@ package Models;
 
 public enum Type {
 Lodging, Travel, Food, Other;
+	private String type;
+	Type(){
+		
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	
 }
