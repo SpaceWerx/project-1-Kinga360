964e509fd78535b7ad263c5186d2e8070a6c937c
Kinga360 <jjadrian1@hotmail.com>
2022-05-09 01:57:32 -0400

    Reimbusement

--- P1/src/main/java/Models/Reimbursement.java
+++ P1/src/main/java/Models/Reimbursement.java
@@ -1,5 +1,112 @@
 package Models;
 
 public class Reimbursement {
-
+	private int ID;
+	private String author;
+	private String resolver;
+	private String Description;
+	private Type type;
+	private Status status;
+	private double amount;
+	public Reimbursement() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public int getID() {
+		return ID;
+	}
+	public void setID(int iD) {
+		ID = iD;
+	}
+	public String getAuthor() {
+		return author;
+	}
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+	public String getResolver() {
+		return resolver;
+	}
+	public void setResolver(String resolver) {
+		this.resolver = resolver;
+	}
+	public String getDescription() {
+		return Description;
+	}
+	public void setDescription(String description) {
+		Description = description;
+	}
+	public Type getType() {
+		return type;
+	}
+	public void setType(Type type) {
+		this.type = type;
+	}
+	public Status getStatus() {
+		return status;
+	}
+	public void setStatus(Status status) {
+		this.status = status;
+	}
+	public double getAmount() {
+		return amount;
+	}
+	public void setAmount(double amount) {
+		this.amount = amount;
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((Description == null) ? 0 : Description.hashCode());
+		result = prime * result + ID;
+		long temp;
+		temp = Double.doubleToLongBits(amount);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		result = prime * result + ((author == null) ? 0 : author.hashCode());
+		result = prime * result + ((resolver == null) ? 0 : resolver.hashCode());
+		result = prime * result + ((status == null) ? 0 : status.hashCode());
+		result = prime * result + ((type == null) ? 0 : type.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Reimbursement other = (Reimbursement) obj;
+		if (Description == null) {
+			if (other.Description != null)
+				return false;
+		} else if (!Description.equals(other.Description))
+			return false;
+		if (ID != other.ID)
+			return false;
+		if (Double.doubleToLongBits(amount) != Double.doubleToLongBits(other.amount))
+			return false;
+		if (author == null) {
+			if (other.author != null)
+				return false;
+		} else if (!author.equals(other.author))
+			return false;
+		if (resolver == null) {
+			if (other.resolver != null)
+				return false;
+		} else if (!resolver.equals(other.resolver))
+			return false;
+		if (status != other.status)
+			return false;
+		if (type != other.type)
+			return false;
+		return true;
+	}
+	@Override
+	public String toString() {
+		return "Reimbursement [ID=" + ID + ", author=" + author + ", resolver=" + resolver + ", Description="
+				+ Description + ", type=" + type + ", status=" + status + ", amount=" + amount + "]";
+	}
+	
 }
